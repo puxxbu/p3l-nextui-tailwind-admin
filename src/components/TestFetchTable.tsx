66f236dbf0f010d04d681935090d76af49fb67b3
puxxbu <pusswarog@gmail.com>
2023-09-06 20:27:20 +0700

    fetch data table done from PZN api

--- src/components/TestFetchTable.tsx
+++ src/components/TestFetchTable.tsx
@@ -8,6 +8,7 @@ import {
   TableCell,
   Pagination,
   getKeyValue,
+  Spinner,
 } from '@nextui-org/react';
 import { users2 as users } from '../data/data';
 
@@ -26,21 +27,33 @@ interface Contact {
 export default function App() {
   const [page, setPage] = React.useState(1);
   const [items, setItems] = React.useState<Contact[]>([]);
-  const { status, data, error, isFetching, isPreviousData } = useQuery({
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isPreviousData,
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ['projects', page],
     queryFn: () => fetchContacts(page),
     keepPreviousData: true,
     staleTime: 5000,
   });
 
+  console.log(page);
+
+  React.useEffect(() => {
+    refetch();
+  }, [page, refetch]);
+
   const rowsPerPage = 10;
 
   const pages = data?.paging.total_page || 1;
 
-  console.log(pages);
-
   React.useEffect(() => {
-    const start = (page - 1) * rowsPerPage;
+    const start = 0;
     const end = start + rowsPerPage;
 
     if (data?.data) {
@@ -48,42 +61,45 @@ export default function App() {
     }
   }, [page, data?.data]);
 
-  return (
-    <Table
-      aria-label="Example table with client side pagination"
-      bottomContent={
-        <div className="flex w-full justify-center">
-          <Pagination
-            isCompact
-            showControls
-            showShadow
-            color="secondary"
-            page={page}
-            total={pages}
-            onChange={(page) => setPage(page)}
-          />
-        </div>
-      }
-      classNames={{
-        wrapper: 'min-h-[222px]',
-      }}
-    >
-      <TableHeader>
-        <TableColumn key="id">NAME</TableColumn>
-        <TableColumn key="role">ROLE</TableColumn>
-        <TableColumn key="status">STATUS</TableColumn>
-        <TableColumn key="status">STATUS</TableColumn>
-        <TableColumn key="status">STATUS</TableColumn>
-      </TableHeader>
-      <TableBody items={items}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => (
-              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-            )}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
+  if (isLoading) {
+    return <Spinner color="primary" />;
+  } else {
+    return (
+      <Table
+        aria-label="Example table with client side pagination"
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color="secondary"
+              page={page}
+              total={pages}
+              onChange={(page) => setPage(page)}
+            />
+          </div>
+        }
+        classNames={{
+          wrapper: 'min-h-[222px]',
+        }}
+      >
+        <TableHeader>
+          <TableColumn key="id">NAME</TableColumn>
+          <TableColumn key="first_name">FIRST NAME</TableColumn>
+          <TableColumn key="last_name">LAST NAME</TableColumn>
+          <TableColumn key="email">EMAIL</TableColumn>
+        </TableHeader>
+        <TableBody items={items}>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
+  }
 }
